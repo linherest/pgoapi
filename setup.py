4a63458c0bddaa06252fb1c5331765174318d0cf
Linherest <linherest@users.noreply.github.com>
2021-02-13 22:31:07 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@
 
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 setup_dir = os.path.dirname(os.path.realpath(__file__))
 path_req = os.path.join(setup_dir, 'requirements.txt')
