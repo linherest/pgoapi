e3bdce71b06c099663e9796c8df166883059edd9
Linherest <linherest@users.noreply.github.com>
2021-02-13 22:47:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup_dir = os.path.dirname(os.path.realpath(__file__))
 path_req = os.path.join(setup_dir, 'requirements.txt')
 install_reqs = parse_requirements(path_req, session=False)
 
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = [str(ir.requirement) for ir in install_reqs]
 
 setup(name='pgoapi',
       author = 'tjado',
